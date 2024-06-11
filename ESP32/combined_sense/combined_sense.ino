805816df20b1eb9e384ffb6d34d512b4d66ac08a
barbarapasalic <barb.pasalic@gmail.com>
2024-06-11 19:34:30 +0200

    Final version

--- ESP32/combined_sense/combined_sense.ino
+++ ESP32/combined_sense/combined_sense.ino
@@ -2,26 +2,30 @@
 #include <PubSubClient.h>
 #include <Arduino.h>
 #include <Ultrasonic.h>
+ 
+const char* ssid = "Tina's Galaxy A54 5G";
+const char* password = "hwvw7kxsaj6728x";
+const char* mqtt_server = "192.168.223.218";
 
-const char* ssid = "ssid";
-const char* password = "1234";
-const char* mqtt_server = "192.168.5.11";
-const int publish_limit = 10; 
 // Define pins for the ultrasonic sensor
 #define TRIGGER_PIN  2
 #define ECHO_PIN     3
 #define LED_PIN      9
 #define PIR_PIN      10
 
+#define TOPIC_MOTION "home/back-door/sensor/motion"
+#define TOPIC_DISTANCE "home/back-door/sensor/ultrasonic"
+#define TOPIC_LED "home/back-door/actuator/buzzer"
+ 
 int pirState = LOW;
-int val=0; 
-int counter=0;
+int val=0;
+ 
 // Define an object of the Ultrasonic class
 Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
-
+ 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
+ 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
@@ -29,9 +33,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
   payload[length] = '\0'; // Null-terminate the payload
   String message = String((char*)payload);
   Serial.println(message);
-
-  if (String(topic) == "home/sensor/led") {
-    if (message == "true") {
+ 
+  if (String(topic) == TOPIC_LED) {
+    if (message == "ON") {
       Serial.println("Turning LED on for 5 seconds");
       digitalWrite(LED_PIN, HIGH);
       delay(5000);
@@ -39,12 +43,12 @@ void callback(char* topic, byte* payload, unsigned int length) {
     }
   }
 }
-
+ 
 void setup_wifi() {
   Serial.begin(115200);
   delay(10);
   Serial.println("Scanning for available networks...");
-
+ 
   int n = WiFi.scanNetworks();
   Serial.println("Scan complete.");
   if (n == 0) {
@@ -66,22 +70,17 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
   WiFi.begin(ssid, password);
-  
   int attempts = 0;
   const int maxAttempts = 40; // Try for 20 seconds
-  
   while (WiFi.status() != WL_CONNECTED && attempts < maxAttempts) {
     delay(500);
     Serial.print(".");
     attempts++;
-    
     // Print the Wi-Fi status code
     Serial.print(" Status: ");
     Serial.println(WiFi.status());
   }
-  
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println("");
     Serial.println("WiFi connected");
@@ -92,14 +91,14 @@ void setup_wifi() {
     Serial.println("Failed to connect to WiFi");
   }
 }
-
+ 
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect with a client ID
     if (client.connect("ESP32Client")) {
       Serial.println("connected");
-      client.subscribe("home/sensor/led"); // Subscribe to the LED topic
+      client.subscribe(TOPIC_LED); // Subscribe to the LED topic
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -108,7 +107,7 @@ void reconnect() {
     }
   }
 }
-
+ 
 void setup() {
   // Initialize serial communication
   Serial.begin(115200);
@@ -119,49 +118,38 @@ void setup() {
   pinMode(PIR_PIN, INPUT);
   digitalWrite(LED_PIN, LOW);
 }
-
+ 
 void loop() {
    if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
+ 
   val = digitalRead(PIR_PIN);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
-    counter++;
     if (pirState == LOW) {
       // we have just turned on
       Serial.println("Motion detected!");
       // We only want to print on the output change, not state
       pirState = HIGH;
-
+ 
       // Read the distance from the ultrasonic sensor in centimeters
       float distance = ultrasonic.distanceRead(CM);
-
+ 
       // Print the distance to the serial monitor
       Serial.print("Distance: ");
       Serial.print(distance);
       Serial.println(" cm");
-
-      if (distance <= 50) {
-        char distanceStr[10];
-        dtostrf(distance, 1, 2, distanceStr);
-
-        if (client.publish("home/sensor/ultrasonic", distanceStr)) {
-          Serial.println("Publish successful");
-        } else {
-          Serial.println("Publish failed");
-        }
+ 
+      char distanceStr[10];
+      dtostrf(distance, 1, 2, distanceStr);
+ 
+      if (client.publish(TOPIC_MOTION, "Motion detected") && client.publish(TOPIC_DISTANCE, distanceStr)) {
+	      Serial.println("Publish successful");
       } else {
-        if (counter == publish_limit) {
-          counter = 0;
-          client.publish("home/sensor/ultrasonic", "0");
-        }
-        Serial.println("Distance is greater than 50 cm, not publishing.");
+	      Serial.println("Publish failed");
       }
-
-    }
-    
+   }
   } else {
     if (pirState == HIGH){
           // we have just turned of
